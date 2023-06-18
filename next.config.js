96c85ceebf4b9011ae1a6954b76fb69e65c9e13b
Timz-creator <timarleyf@gmail.com>
2023-06-18 13:42:52 +0100

    env.local file added

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    MONGODB_URL: 'mongodb+srv://timarleyf:0q3VwQ7SEG1ycChe@cluster0.y1hvkum.mongodb.net/ecommerce?retryWrites=true&w=majority',
+    
+  }
+}
